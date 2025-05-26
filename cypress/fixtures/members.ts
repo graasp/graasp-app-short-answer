c7e6181c7765fd6267b78068319f3495a11b8499
Jérémy La Scala <jeremy.lascala@epfl.ch>
2025-05-26 10:37:06 +0200

    test: fix fixtures with Graasp SDK v5

--- cypress/fixtures/members.ts
+++ cypress/fixtures/members.ts
@@ -1,30 +1,27 @@
-import { CompleteMember, MemberType } from '@graasp/sdk';
+import { AccountType, CompleteAccount } from '@graasp/sdk';
 
-export const MEMBERS: { [key: string]: CompleteMember } = {
+export const MEMBERS: { [key: string]: CompleteAccount & { email: string } } = {
   ANNA: {
     id: '0f0a2774-a965-4b97-afb4-bccc3796e060',
     name: 'anna',
-    type: MemberType.Individual,
+    type: AccountType.Individual,
     email: 'anna@gmail.com',
-    extra: {},
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
   },
   BOB: {
     id: '1f0a2774-a965-4b97-afb4-bccc3796e060',
     name: 'bob',
-    type: MemberType.Individual,
+    type: AccountType.Individual,
     email: 'bob@gmail.com',
-    extra: {},
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
   },
   CHARLIE: {
     id: '2f0a2774-a965-4b97-afb4-bccc3796e060',
     name: 'charlie',
-    type: MemberType.Individual,
+    type: AccountType.Individual,
     email: 'charlie@gmail.com',
-    extra: {},
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
   },
